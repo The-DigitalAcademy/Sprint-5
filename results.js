09cce0e274a7829c61cb0761b902019c60b24221
Nathandiv <k.n.hlungwani@gmail.com>
2024-06-12 15:25:50 +0200

    add funct() for bmi

--- results.js
+++ results.js
@@ -1,6 +1,6 @@
-let Name = prompt("Enter your Name");
-let Gender = prompt("Enter Gender");
-let Age = Number(prompt("Enter your age "));
+let Name = String(document.getElementById("Fname").value);
+let Gender = Number(document.getElementById("gender").value);
+let Age =Number(document.getElementById("age").value);
 let group1 = `Roadwork   (4km) \n Squarts  (20 reps) \n Push ups (25 reps)`;
 let group2 = `Roadwork   (3km) \n Squarts  (40 reps) \n Push ups (20 reps)`;
 let group3 = `Roadwork   (8km) \n Squarts  (40 reps) \n Push ups (60 reps)`;
@@ -10,7 +10,19 @@ let group6 = `Roadwork   (4km) \n Squarts  (65 reps) \n Push ups (25 reps)`;
 let group7 = `Roadwork   (2km) \n Squarts  (10 reps) \n Push ups (10 reps)`;
 let group8 = `Roadwork   (1km) \n Squarts  (20 reps) \n Push ups (5 reps)`;
 let group9 = `Roadwork   (500m) \n Squarts  (10 reps) \n Push ups (10 reps)`;
-let weight = Number(prompt("Enter your weight"));
+let weight = Number(document.getElementById(" weight").value);
 let height = parseFloat(prompt("Enter your height"));
 let bmi = Number((weight / ((height)^2)))
 let weightResult;
+
+function calc() {
+    if (bmi < 18.5) {
+        weightResult = "underweight";
+    } else if (bmi >= 18.5 && bmi < 24.9) {
+        weightResult = "healthy"; 
+    } else if (bmi >= 25.0 && bmi < 29.9) {
+        weightResult = "overweight"; 
+    } else {
+        weightResult = "obese";
+    }
+}
