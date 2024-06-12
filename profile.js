cdbcce9f3ccf68ebb0e58187295a409746fe9114
Nellark <nelakulati@gmail.com>
2024-06-12 10:43:02 +0200

    aligning login page and nav bar

--- profile.js
+++ profile.js
@@ -6,44 +6,38 @@ function gymApp() {
     let height = Number(document.getElementById('height').value);
     let bmi = weight / (height * height);
     let weightresults;
-   
 
     if (bmi < 18.5) {
-        weightresults = 'Underweight )';
-    }
-    else if (bmi >= 18.5 && bmi < 24.9) {
-        weightresults = 'Healthy )';
-    }
-    else if (bmi >= 25 && bmi < 29.9) {
-        weightresults = 'Overweight )';
-    }
-    else if (bmi >= 30) {
-        weightresults = 'Obese )';
+        weightresults = 'Underweight';
+    } else if (bmi >= 18.5 && bmi < 24.9) {
+        weightresults = 'Healthy';
+    } else if (bmi >= 25 && bmi < 29.9) {
+        weightresults = 'Overweight';
+    } else if (bmi >= 30) {
+        weightresults = 'Obese';
     }
 
     if (fname === '' || gender === '' || age === '' || weight === '' || height === '') {
         alert('Please fill in all the fields');
     } else {
         // If all fields are filled, proceed with form submission
-        document.getElementById('display').innerHTML = 'Name: ' + name + '<br>' +
-            'Gender: ' + gender + '<br>' +
-            'Age: ' + age + '<br>' +
-            'Weight: ' + weight + '<br>' +
-            'Height: ' + height;
+        // document.getElementById('display').innerHTML = 'Name: ' + fname + '<br>' +
+        //     'Gender: ' + gender + '<br>' +
+        //     'Age: ' + age + '<br>' +
+        //     'Weight: ' + weight + '<br>' +
+        //     'Height: ' + height;
     }
 
-
-
-    document.getElementById('display').innerHTML = 'Hi ' + fname + ', Here is your profile:<br>' +
+    document.getElementById('display').innerHTML += '<br>Hi ' + fname + ', Here is your profile:<br>' +
                                                       'Age      : ' + age + '<br>' +
                                                       'Gender   : ' + gender + '<br>' +
                                                       'Weight   : ' + weight + ' KG <br>'+
                                                       'BMI      : ' + bmi.toFixed(1) + 
-                                                      '(You are ' + weightresults; + '<br>' 
+                                                      ' (You are ' + weightresults + ')<br>'; 
 
     if (age >= 15 && age < 21) { 
         if (gender === 'Male') {
-            document.getElementById('display').innerHTML += 'This is an exercise routine tailored specifically for you:' +
+            document.getElementById('display').innerHTML += 'This is an exercise routine tailored specifically for you:<br>' +
                                                              'Roadwork - 4KM per day<br>' +
                                                              'Squats - 20 per day<br>' +
                                                              'Push Ups - 25 per day';
@@ -82,6 +76,7 @@ function gymApp() {
                                                              'Push Ups - 20 per day';
         }
     }
+
     if (age >= 50 && age < 100) { 
         if (gender === 'Male') {
             document.getElementById('display').innerHTML += '<br>This is an exercise routine tailored specifically for you:<br>' +
