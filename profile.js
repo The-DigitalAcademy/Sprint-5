e1f6132492cbd063da7b16af010c66a7ef972726
Nellark <nelakulati@gmail.com>
2024-06-12 15:42:16 +0200

    I dded the fuctions of the submit button

--- profile.js
+++ profile.js
@@ -1,93 +1,20 @@
 function gymApp() {
     let fname = document.getElementById('fname').value;
-    let gender = document.getElementById('gender').value;
-    let age = Number(document.getElementById('age').value); 
-    let weight = Number(document.getElementById('weight').value);
-    let height = Number(document.getElementById('height').value);
-    let bmi = weight / (height * height);
-    let weightresults;
+    let surname = document.getElementById('surname').value;
+    let email = document.getElementById('email').value;
+    let phone = document.getElementById('phone').value;
+    let id = document.getElementById('id').value;
+    let password1 = document.getElementById('password1').value;
+    let password2 = document.getElementById('password2').value;
+    let location = document.getElementById('location').value;
 
-    if (bmi < 18.5) {
-        weightresults = 'Underweight';
-    } else if (bmi >= 18.5 && bmi < 24.9) {
-        weightresults = 'Healthy';
-    } else if (bmi >= 25 && bmi < 29.9) {
-        weightresults = 'Overweight';
-    } else if (bmi >= 30) {
-        weightresults = 'Obese';
-    }
-
-    if (fname === '' || gender === '' || age === '' || weight === '' || height === '') {
+    if (fname === '' || surname === '' || email === '' || phone === '' 
+        || id === '' || password1 === '' || password2 === '' || location === '') {
         alert('Please fill in all the fields');
+    } else if (password1 !== password2) {
+        alert('Passwords do not match');
     } else {
-        // If all fields are filled, proceed with form submission
-        // document.getElementById('display').innerHTML = 'Name: ' + fname + '<br>' +
-        //     'Gender: ' + gender + '<br>' +
-        //     'Age: ' + age + '<br>' +
-        //     'Weight: ' + weight + '<br>' +
-        //     'Height: ' + height;
-    }
-
-    document.getElementById('display').innerHTML += '<br>Hi ' + fname + ', Here is your profile:<br>' +
-                                                      'Age      : ' + age + '<br>' +
-                                                      'Gender   : ' + gender + '<br>' +
-                                                      'Weight   : ' + weight + ' KG <br>'+
-                                                      'BMI      : ' + bmi.toFixed(1) + 
-                                                      ' (You are ' + weightresults + ')<br>'; 
-
-    if (age >= 15 && age < 21) { 
-        if (gender === 'Male') {
-            document.getElementById('display').innerHTML += 'This is an exercise routine tailored specifically for you:<br>' +
-                                                             'Roadwork - 4KM per day<br>' +
-                                                             'Squats - 20 per day<br>' +
-                                                             'Push Ups - 25 per day';
-        } else if (gender === 'Female') {
-            document.getElementById('display').innerHTML += '<br>This is an exercise routine tailored specifically for you:<br>' +
-                                                             'Roadwork - 3KM per day<br>' +
-                                                             'Squats - 40 per day<br>' +
-                                                             'Push Ups - 20 per day';
-        }
-    }
-
-    if (age >= 20 && age < 31) { 
-        if (gender === 'Male') {
-            document.getElementById('display').innerHTML += '<br>This is an exercise routine tailored specifically for you:<br>' +
-                                                             'Roadwork - 8KM per day<br>' +
-                                                             'Squats - 20 per day<br>' +
-                                                             'Push Ups - 50 per day';
-        } else if (gender === 'Female') {
-            document.getElementById('display').innerHTML += '<br>This is an exercise routine tailored specifically for you:<br>' +
-                                                             'Roadwork - 6KM per day<br>' +
-                                                             'Squats - 40 per day<br>' +
-                                                             'Push Ups - 50 per day';
-        }
-    }
-
-    if (age >= 30 && age < 51) { 
-        if (gender === 'Male') {
-            document.getElementById('display').innerHTML += '<br>This is an exercise routine tailored specifically for you:<br>' +
-                                                             'Roadwork - 5KM per day<br>' +
-                                                             'Squats - 20 per day<br>' +
-                                                             'Push Ups - 30 per day';
-        } else if (gender === 'Female') {
-            document.getElementById('display').innerHTML += '<br>This is an exercise routine tailored specifically for you:<br>' +
-                                                             'Roadwork - 3KM per day<br>' +
-                                                             'Squats - 30 per day<br>' +
-                                                             'Push Ups - 20 per day';
-        }
-    }
-
-    if (age >= 50 && age < 100) { 
-        if (gender === 'Male') {
-            document.getElementById('display').innerHTML += '<br>This is an exercise routine tailored specifically for you:<br>' +
-                                                             'Roadwork - 2KM per day<br>' +
-                                                             'Squats - 10 per day<br>' +
-                                                             'Push Ups - 15 per day';
-        } else if (gender === 'Female') {
-            document.getElementById('display').innerHTML += '<br>This is an exercise routine tailored specifically for you:<br>' +
-                                                             'Roadwork - 1.5KM per day<br>' +
-                                                             'Squats - 15 per day<br>' +
-                                                             'Push Ups - 10 per day';
-        }
+        // Redirect to home.html
+        window.location.href = 'home.html';
     }
 }
