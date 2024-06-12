155fb14d49dfd9a822fac75f817af52c6c5f9966
Nathandiv <k.n.hlungwani@gmail.com>
2024-06-12 15:01:25 +0200

    added varibles for the App Results button

---
+++ results.js
@@ -0,0 +1,16 @@
+let Name = prompt("Enter your Name");
+let Gender = prompt("Enter Gender");
+let Age = Number(prompt("Enter your age "));
+let group1 = `Roadwork   (4km) \n Squarts  (20 reps) \n Push ups (25 reps)`;
+let group2 = `Roadwork   (3km) \n Squarts  (40 reps) \n Push ups (20 reps)`;
+let group3 = `Roadwork   (8km) \n Squarts  (40 reps) \n Push ups (60 reps)`;
+let group4 = `Roadwork   (6km) \n Squarts  (60 reps) \n Push ups (35 reps)`;
+let group5 = `Roadwork   (5km) \n Squarts  (30 reps) \n Push ups (30 reps)`;
+let group6 = `Roadwork   (4km) \n Squarts  (65 reps) \n Push ups (25 reps)`;
+let group7 = `Roadwork   (2km) \n Squarts  (10 reps) \n Push ups (10 reps)`;
+let group8 = `Roadwork   (1km) \n Squarts  (20 reps) \n Push ups (5 reps)`;
+let group9 = `Roadwork   (500m) \n Squarts  (10 reps) \n Push ups (10 reps)`;
+let weight = Number(prompt("Enter your weight"));
+let height = parseFloat(prompt("Enter your height"));
+let bmi = Number((weight / ((height)^2)))
+let weightResult;
